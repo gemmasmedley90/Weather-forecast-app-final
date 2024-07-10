3cef9013c18008328527a8b41972646dda47d23c
gemmasmedley90 <gemmasmedley90@gmail.com>
2024-07-10 20:27:43 +0100

    Added weather icon change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -30,7 +32,7 @@ function formatDate(date) {
   ];
   let day = days[date.getDay()];
 
-  if (minutes < 10){
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
